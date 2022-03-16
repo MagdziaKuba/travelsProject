1bb3ca734d8fa6f3d3f0a12926ac15bcfa68f578
Magda <mkubacomperia@gmail.com>
2022-03-16 17:03:28 +0100

    calendar

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -15,6 +15,14 @@ public class HotelSearch {
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         Thread.sleep(3000);
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
-        
+        driver.findElement(By.name("checkin")).sendKeys("18/05/2022");
+        //driver.findElement(By.name("checkout")).sendKeys("11/07/2022");
+        /*driver.findElement(By.name("checkin")).click();
+        driver.findElement(By.xpath("//td[@class='day ' and text()='17']")).click();*/
+        driver.findElement(By.name("checkout")).click();
+        //driver.findElement(By.xpath("//td[@class='day ' and text()='22']")).click();
+        //musi byc findElements do stream()
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']")).stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el ->el.click());
+        //dok.
     }
 }
