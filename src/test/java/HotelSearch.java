8e96511501f3b3ea7f32de8e70a5a95fe7b9fe3c
Magda <mkubacomperia@gmail.com>
2022-03-16 13:22:32 +0100

    city

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class HotelSearch {
+    @Test
+    public void searchHotel() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+        
+    }
+}
