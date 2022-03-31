4c02e215e21d58b9e27f53525d6d8959811e059a
Magda <mkubacomperia@gmail.com>
2022-03-31 10:57:13 +0200

    DriverFactory dla firefox

--- src/test/java/pl/seleniumdemo/tests/BaseTests.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTests.java
@@ -4,14 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pl.seleniumdemo.utils.DriverFactory;
 
 public class BaseTests {
     public WebDriver driver;
 
     @BeforeMethod
     public void setup(){
-        WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+        //WebDriverManager.chromedriver().setup();
+        //driver=new ChromeDriver();
+        driver= DriverFactory.getDriver("firefox");
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
     }
